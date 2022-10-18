4e5d28f57479dcaa730b08ac0cd1b4c33b0df011
Jubjub422 <ross.stovenour@gmail.com>
2022-10-18 14:17:16 -0500

    adjusting for different macine

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -9,7 +9,7 @@ export const Login = () => {
     const history = useHistory()
 
     const existingManagerCheck = () => {
-        return fetch(`http://localhost:8088/customers?email=${email}`)
+        return fetch(`http://localhost:8088/managers?email=${email}`)
             .then(res => res.json())
             .then(manager => manager.length ? manager[0] : false)
     }
@@ -19,7 +19,7 @@ export const Login = () => {
         existingManagerCheck()
             .then(exists => {
                 if (exists) {
-                    localStorage.setItem("honey_customer", exists.id)
+                    localStorage.setItem("island_manager", exists.id)
                     history.push("/")
                 } else {
                     existDialog.current.showModal()
@@ -36,7 +36,7 @@ export const Login = () => {
 
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Honey Rae Repairs</h1>
+                    <h1>Island Manager</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
                         <label htmlFor="inputEmail"> Email address </label>
